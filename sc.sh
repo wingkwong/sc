4574ddda2a4156caec55285cbac46cb8fa5b54cf
wingkwong <wingkwong.code@gmail.com>
2019-07-18 23:36:10 +0800

    Create sc.sh

---
+++ sc.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+usage()
+{
+    cat << EOF
+--------------------------------------------------------
+Usage:
+--------------------------------------------------------
+To enable an alias module   :     sc -e [alias_module]
+To disable an alias module  :     sc -d [alias_module]
+To check the usage          :     sc -h 
+
+EOF
+}
+
+enable_alias_module()
+{
+    validate_alias_module $1
+}
+
+disable_alias_module()
+{
+    validate_alias_module $1
+}
+
+validate_alias_module()
+{
+    if [ -z $1 ]; then
+        echo "<alias module> is required."
+        exit 1
+    fi
+}
+
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -e | --enable )         shift
+                                enable_alias_module $1
+                                ;;
+        -d | --disable )        disable_alias_module $1
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
